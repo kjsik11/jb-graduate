5de838d4b71125ee53903542545fb017fee68c13
jongsik <kjsik11@gmail.com>
2021-05-30 13:56:58 +0900

    make breadCrumb component
    add eventlog template

--- pages/docs/eventlog.tsx
+++ pages/docs/eventlog.tsx
@@ -1,30 +1,74 @@
 import React from 'react';
+import cn from 'classnames';
 
 // components
 import Dashboard from '@components/layout/Dashboard';
-import Button from '@components/ui/Button';
-import Link from '@components/ui/Link';
+import BreadCrumb from '@components/ui/BreadCrumb';
+import { ChevronRightIcon } from '@heroicons/react/solid';
+
+const BreadPages = [
+  {
+    name: 'docs',
+    href: '/docs',
+    current: false,
+  },
+  {
+    name: 'eventlog',
+    href: '/docs/eventlog',
+    current: false,
+  },
+];
+
+const SideBarItems = [
+  {
+    title: 'test1',
+    key: 'test1',
+  },
+  {
+    title: 'test2',
+    key: 'test2',
+  },
+  {
+    title: 'test3',
+    key: 'test3',
+  },
+  {
+    title: 'test4',
+    key: 'test4',
+  },
+];
 
 const EventLogDocsPage = () => {
   return (
-    <>
-      <div className="h-full">
-        <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span className="block text-lightBlue-400 pt-4">
-              준비중인 페이지입니다
-            </span>
-          </h2>
-          <div className="mt-8 flex justify-center">
-            <div className="inline-flex rounded-md shadow">
-              <Link href="/">
-                <Button>홈으로가기</Button>
-              </Link>
-            </div>
+    <div className="flex">
+      <div className="w-64 bg-gray-100 fixed top-0 left-[112px] h-screen p-2">
+        <p className="text-xl ml-2 mb-4">Event Log</p>
+        {SideBarItems.map((sideBarItem, idx) => (
+          <div
+            key={`${sideBarItem.key}-${idx}`}
+            className="ml-4 my-4 text-gray-500 hover:text-black"
+          >
+            <a href={`#${sideBarItem.key}`}>
+              <span className="text-2xl p-2">&middot;</span> {sideBarItem.title}
+            </a>
+          </div>
+        ))}
+      </div>
+      <div className="w-full pl-[268px] m-8">
+        <BreadCrumb pages={BreadPages} />
+        <div className="max-w-5xl mx-auto overflow-y-scroll mt-8">
+          <div id="test2" className="h-screen">
+            hi2
+          </div>
+          <div id="test3" className="h-screen">
+            hi3
+          </div>
+          <div id="test4" className="h-screen">
+            hi4
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
