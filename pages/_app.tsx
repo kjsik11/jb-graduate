24de8005b58e879cc51ace4a55effd3ed1c3ba77
jongsik <kjsik11@gmail.com>
2021-06-02 17:34:13 +0900

    add sw

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,18 +29,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
 
   React.useEffect(() => {
     if ('serviceWorker' in navigator) {
-      // Register a service worker hosted at the root of the
-      // site using the default scope.
-      navigator.serviceWorker.register('/sw.js').then(
-        function (registration) {
-          console.log('Service worker registration succeeded:', registration);
-        },
-        /*catch*/ function (error) {
-          console.log('Service worker registration failed:', error);
-        },
-      );
-    } else {
-      console.log('Service workers are not supported.');
+      navigator.serviceWorker.register('/sw.js');
     }
   }, []);
 
